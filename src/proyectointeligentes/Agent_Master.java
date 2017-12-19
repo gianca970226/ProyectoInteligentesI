24eea9e049ad3b2de6a17e6ec83877e8fe62c163
lorenagiraldo <lorenagiraldo1031@gmail.com>
2017-12-18 21:46:38 -0500

    commit18-12

---
+++ src/proyectointeligentes/Agent_Master.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectointeligentes;
+
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+
+/**
+ *
+ * @author Ander
+ */
+public class Agent_Master {
+
+    private static ContainerController containerController;
+    Runtime runtime;
+    private final ProfileImpl profile;
+    AgentController Agente;
+
+    public Agent_Master() {
+        runtime = Runtime.instance();
+        profile = new ProfileImpl();
+        profile.setParameter(ProfileImpl.MAIN_HOST, "localhost");
+        profile.setParameter(ProfileImpl.MAIN_PORT, "1099");
+        profile.setParameter(ProfileImpl.MAIN, "true");
+
+    }
+
+    public void initController() {
+        containerController = runtime.createMainContainer(profile);
+    }
+
+    public void initAgent(String name, String agentClass, Object[] args) {
+        try {
+            Agente = containerController.createNewAgent(name, agentClass, args);
+            Agente.start();
+        } catch (Exception e) {
+            System.out.println("error"+e);
+        }
+    }
+
+    public AgentController getAgente() {
+        return Agente;
+    }
+
+    public void setAgente(AgentController Agente) {
+        this.Agente = Agente;
+    }
+
+
+    
+
+}
